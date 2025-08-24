85e2664d82624c3eb184c06d715351dd0687f4be
Ryan Hyun Choi <ryan.h.choi@gmail.com>
2025-08-24 23:25:05 +0900

    Update hdd_info
    Signed-off-by: Ryan Hyun Choi <ryan.h.choi@gmail.com>

--- nas/hdd_info.sh
+++ nas/hdd_info.sh
@@ -16,10 +16,10 @@ echo "==================="
 echo "Important HDD Info "
 echo "==================="
 # 8t
-sudo smartctl -A /dev/disk/by-id/ata-WDC_WD80EMAZ-00WJTA0_7SGHG8UC | egrep -i "temperature|reallocated_sector|current_pending|offline_uncorr|power_on"
+sudo smartctl -a /dev/disk/by-id/ata-WDC_WD80EMAZ-00WJTA0_7SGHG8UC | egrep -i "Device\ Model|SMART\ overall-health|temperature|reallocated_sector|current_pending|offline_uncorr|power_on"
 # 8t-b
-sudo smartctl -A /dev/disk/by-id/ata-WDC_WD80EMAZ-00WJTA0_7SH0T09C | egrep -i "temperature|reallocated_sector|current_pending|offline_uncorr|power_on"
+sudo smartctl -a /dev/disk/by-id/ata-WDC_WD80EMAZ-00WJTA0_7SH0T09C | egrep -i "Device\ Model|SMART\ overall-health|temperature|reallocated_sector|current_pending|offline_uncorr|power_on"
 # 3t
-sudo smartctl -A /dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0077880 | egrep -i "temperature|reallocated_sector|current_pending|offline_uncorr|power_on"
+sudo smartctl -a /dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0077880 | egrep -i "Device\ Model|SMART\ overall-health|temperature|reallocated_sector|current_pending|offline_uncorr|power_on"
 # 3t-b
-sudo smartctl -A /dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0092210 | egrep -i "temperature|reallocated_sector|current_pending|offline_uncorr|power_on"
+sudo smartctl -a /dev/disk/by-id/ata-WDC_WD30EFRX-68EUZN0_WD-WCC4N0092210 | egrep -i "Device\ Model|SMART\ overall-health|temperature|reallocated_sector|current_pending|offline_uncorr|power_on"
